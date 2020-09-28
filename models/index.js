888c0e0dee186aed11074f6bb3deb0d0b31b1ecf
n-dre <contact@oli-branch.com>
2020-09-28 17:43:22 -0400

    New files

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const AuthorsModel = require('./authors')
+const GenresModel = require('./genres')
+const NovelsModel = require('./novels')
+const NovelsGenresModel = require('./novelsGenres')
+
+const connection = new Sequelize('novel', 'novel', '4218', {
+  host: 'localhost', dialect: 'mysql'
+})
+
+const Authors = AuthorsModel(connection, Sequelize)
+const Genres = GenresModel(connection, Sequelize)
+const Novels = NovelsModel(connection, Sequelize, Authors)
+const NovelsGenres = NovelsGenresModel(connection, Sequelize, Genres, Novels)
+
+Novels.belongsTo(Authors)
+Authors.hasMany(Novels)
+
+Genres.belongsToMany(Novels, { through: NovelsGenres })
+Novels.belongsToMany(Genres, { through: NovelsGenres })
+
+module.exports = {
+  Authors,
+  Genres,
+  Novels,
+  NovelsGenres,
+}
+
